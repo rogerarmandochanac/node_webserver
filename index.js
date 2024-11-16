54621b0f93713c371779afae2214a65c65fc0330
rogerarmandochanac <armandoroger007@gmail.com>
2024-11-16 00:18:47 -0600

    Sirviendo contenido estatico

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static('public'))
 
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get("*", (req, res)=>{
+    res.sendFile(__dirname + "/public/404.html");
+})
+
 app.listen(8080, ()=>{
     console.log("Servidor escuchando desde el puerto 8080");
 })
